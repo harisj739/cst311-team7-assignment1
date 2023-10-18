c98d9226ce4b60fa12b552ea7c2c45cf5928520f
Haris <126624444+harisj739@users.noreply.github.com>
2023-10-18 11:10:30 -0700

    Finished legacy_network.py (comments in code may change)
    The following changes were made:
          1. Moved lines 28 and 29 before lines 26 and 27 (initialize 
              switches before routers) to remove Python errors.
          2. Changed IP addresses of the hosts h3 and h4 to fit the 
              address space’s subnet mask in Lines 40 & 41 since the 
              assignment requires the East Coast Network to have an 
              address space of 10.0.c.d/24 (where we chose c is 1).
          3. Assigned IP addresses to the routing interfaces from the 
              links from routers r3 to r4 and routers r4 to r5 in Lines 51 & 
              52 so forwarding between the routers r3 and r5 can occur 
              through the two interfaces of r4.
          4. Added default routes to each of the hosts from Line 39 - 
              43 to connect the subnets to their respective routers (r3 
              and r5) to create a path for the hosts in different subnets to 
              ping one another.
           5. Attached each switch is connected to the correct routing 
               interface through its IP address in Lines 50 & 51 to allow 
               switches s1 and s2 to forward packets to their respective 
               routers.
           6. Created static routes that add subnet masks to each 
               router, forging a path from the West Coast Network to the 
               East Coast Network and enables pinging between hosts in 
               the two different subnets.

--- TPA4/legacy_network.py
+++ TPA4/legacy_network.py
@@ -23,33 +23,51 @@ def myNetwork():
                       port=6633)
 
     info( '*** Add switches\n')
+    # Switches are now properly initialized before the initialization of routers.
     s2 = net.addSwitch('s2', cls=OVSKernelSwitch)
     s1 = net.addSwitch('s1', cls=OVSKernelSwitch)
-    r5 = net.addHost('r5', cls=Node, ip='10.0.1.0')
+    r5 = net.addHost('r5', cls=Node, ip='10.0.1.4/24')
     r5.cmd('sysctl -w net.ipv4.ip_forward=1')
-    r4 = net.addHost('r4', cls=Node, ip='192.168.1.2')
+    r4 = net.addHost('r4', cls=Node, ip='192.168.0.0/30')
     r4.cmd('sysctl -w net.ipv4.ip_forward=1')
-    r3 = net.addHost('r3', cls=Node, ip='10.0.2.0')
+    r3 = net.addHost('r3', cls=Node, ip='10.0.0.4/24')
     r3.cmd('sysctl -w net.ipv4.ip_forward=1')
 
     info( '*** Add hosts\n')
-    h1 = net.addHost('h1', cls=Host, ip='10.0.0.1', defaultRoute=None)
-    h2 = net.addHost('h2', cls=Host, ip='10.0.2.1', defaultRoute=None)
-    h3 = net.addHost('h3', cls=Host, ip='10.0.0.3', defaultRoute=None)
-    h4 = net.addHost('h4', cls=Host, ip='10.0.0.4', defaultRoute=None)
+    # Added default routes to each of the hosts, which connect the two subnets together.
+    h1 = net.addHost('h1', cls=Host, ip='10.0.0.1/24', defaultRoute='via 10.0.0.4')
+    h2 = net.addHost('h2', cls=Host, ip='10.0.0.2/24', defaultRoute='via 10.0.0.4')
+    # Modified the host's IP addresses of h3 and h4 to fit the East Coast Network's address space.
+    h3 = net.addHost('h3', cls=Host, ip='10.0.1.1/24', defaultRoute='via 10.0.1.4')
+    h4 = net.addHost('h4', cls=Host, ip='10.0.1.2/24', defaultRoute='via 10.0.1.4')
 
     info( '*** Add links\n')
     net.addLink(h1, s1)
     net.addLink(h2, s1)
     net.addLink(h3, s2)
     net.addLink(h4, s2)
-    net.addLink(s2, r5)
-    net.addLink(s1, r3)
-    net.addLink(r3, r4)
-    net.addLink(r4, r5)
+    # Modified the following two addLink statement by connecting each switch to its respective routing interface. 
+    net.addLink(s2, r5, intfName2='r5-eth0', params2={ 'ip' : '10.0.1.3/24' })
+    net.addLink(s1, r3, intfName2='r3-eth0', params2={ 'ip' : '10.0.0.3/24' })
+    # Modified the following two addLink statements by adding 2 two-host networks for each link present between the routers.
+    net.addLink(r3, r4, intfName1='r3-eth1', params1={ 'ip' : '192.168.0.1/30' }, intfName2='r4-eth0', params2={ 'ip' : '192.168.0.2/30' })
+    net.addLink(r4, r5, intfName1='r4-eth1', params1={ 'ip' : '192.168.1.1/30' }, intfName2='r5-eth1', params2={ 'ip' : '192.168.1.2/30' })
 
     info( '*** Starting network\n')
     net.build()
+    
+    # Added static routes to interconnect each router's interfaces to connect the two subnets.
+    # Static Routes: r3
+    info(net['r3'].cmd ('ip route add 10.0.1.0/24 via 192.168.0.0 dev r3-eth1'))
+    info(net['r3'].cmd ('ip route add 192.168.1.0/30 via 192.168.0.0 dev r3-eth1'))
+    # Static Routes: r4
+    info(net['r4'].cmd ('ip route add 10.0.0.0/24 via 192.168.0.1 dev r4-eth0'))
+    info(net['r4'].cmd ('ip route add 10.0.1.0/24 via 192.168.1.2 dev r4-eth1'))
+    # Static Routes: r5
+    info(net['r5'].cmd ('ip route add 10.0.0.0/24 via 192.168.1.1 dev r5-eth1'))
+    info(net['r5'].cmd ('ip route add 192.168.0.0/30 via 192.168.1.1 dev r5-eth1'))
+    
+    
     info( '*** Starting controllers\n')
     for controller in net.controllers:
         controller.start()
