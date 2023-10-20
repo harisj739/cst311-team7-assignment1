0a1a08f47716d073c20c1db53c7a0c7fe2f3be10
Anthony Matricia <124718124+anthonymatricia@users.noreply.github.com>
2023-10-19 23:04:21 -1000

    Update certificate_generation.py
    reworked certificate_generation

--- TPA4/certificate_generation.py
+++ TPA4/certificate_generation.py
@@ -1,55 +1,34 @@
-# Generate_certificates.py
+"""Certificate Generation for CST311 Programming Assignment 4"""
+__author__ = "Team 7 - SSS"
+__credits__ = [
+  "Andrew Grant",
+  "Anthony Matricia",
+  "Haris Jilani"
+]
 
-from OpenSSL import crypto
 import subprocess
 
-# Common Names for the servers
-web_server_cn = "www.webpa4.test"
-chat_server_cn = "chat_server.pa4.test"
+# Step 1: Ask for common names
+chat_server_common_name = input("Enter common name for the chat server: ")
+web_server_common_name = input("Enter common name for the web server: ")
 
-# Generate a certificate for the web server
-web_cert = crypto.X509()
-web_cert.set_serial_number(1)
-web_cert.gmtime_adj_notBefore(0)
-web_cert.gmtime_adj_notAfter(365 * 24 * 60 * 60)  # Valid for a year
+# Step 2: Generate private keys
+def generate_private_key(key_filename):
+    subprocess.run(["sudo", "openssl", "genrsa", "-out", key_filename, "2048"])
 
-web_subject = web_cert.get_subject()
-web_subject.CN = web_server_cn
+chat_server_key_filename = "chatserver-key.pem"
+web_server_key_filename = "webserver-key.pem"
 
-key = crypto.PKey()
-key.generate_key(crypto.TYPE_RSA, 2048)
-web_cert.set_pubkey(key)
-web_cert.sign(key, "sha256")
+generate_private_key(chat_server_key_filename)
+generate_private_key(web_server_key_filename)
 
-with open("web_server.crt", "wb") as web_cert_file:
-    web_cert_file.write(crypto.dump_certificate(crypto.FILETYPE_PEM, web_cert))
-with open("web_server.key", "wb") as web_key_file:
-    web_key_file.write(crypto.dump_privatekey(crypto.FILETYPE_PEM, key))
+# Step 3: Generate certificate signing requests
+def generate_certificate_signing_request(key_filename, csr_filename, common_name):
+    subprocess.run(["sudo", "openssl", "req", "-new", "-config", "/etc/ssl/openssl.cnf", "-key", key_filename, "-out", csr_filename, "-subj", f"/C=US/ST=CA/L=Seaside/O=CST311/OU=Networking/CN={common_name}"])
 
-# Generate a certificate for the chat server
-chat_cert = crypto.X509()
-chat_cert.set_serial_number(2)
-chat_cert.gmtime_adj_notBefore(0)
-chat_cert.gmtime_adj_notAfter(365 * 24 * 60 * 60)  # Valid for a year
+chat_server_csr_filename = "chatserver-cert.csr"
+web_server_csr_filename = "webserver-cert.csr"
 
-chat_subject = chat_cert.get_subject()
-chat_subject.CN = chat_server_cn
+generate_certificate_signing_request(chat_server_key_filename, chat_server_csr_filename, chat_server_common_name)
+generate_certificate_signing_request(web_server_key_filename, web_server_csr_filename, web_server_common_name)
 
-key = crypto.PKey()
-key.generate_key(crypto.TYPE_RSA, 2048)
-chat_cert.set_pubkey(key)
-chat_cert.sign(key, "sha256")
-
-with open("chat_server.crt", "wb") as chat_cert_file:
-    chat_cert_file.write(crypto.dump_certificate(crypto.FILETYPE_PEM, chat_cert))
-with open("chat_server.key", "wb") as chat_key_file:
-    chat_key_file.write(crypto.dump_privatekey(crypto.FILETYPE_PEM, key))
-
-# Copy the certificates
-subprocess.call(["cp", "web_server.crt", "/etc/ssl/certs/"])
-subprocess.call(["cp", "web_server.key", "/etc/ssl/private/"])
-subprocess.call(["cp", "chat_server.crt", "/etc/ssl/certs/"])
-subprocess.call(["cp", "chat_server.key", "/etc/ssl/private/"])
-# Add the IP addresses and names of the CN in /etc/hosts.
-subprocess.call(["sudo", "sed", "-i", "5i 127.0.0.1       chat_server.pa4.test", "/etc/hosts"])
-subprocess.call(["sudo", "sed", "-i", "6i 10.0.0.5        www.webpa4.test", "/etc/hosts"])
