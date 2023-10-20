195446eacad2a6c9de71a1d1e0e13daca237310d
Haris <126624444+harisj739@users.noreply.github.com>
2023-10-20 11:39:32 -0700

    Updated certificate_generation.py that works
    I made a number of changes:
         1. Added functionality on Lines 16 and 17 to map the inputted 
            common names with IP addresses into the /etc/hosts file.
         2. Modified the subprocess being run in the generate_key() 
             method on Line 22 to match Lab 6A Step 8.
         3. Removed the key_filename argument from the 
             generate_certificate() method in Line 33 and altered the 
             subprocess being run to match Lab 6A Step 11.

--- TPA4/certificate_generation.py
+++ TPA4/certificate_generation.py
@@ -12,9 +12,14 @@ import subprocess
 chat_server_common_name = input("Enter common name for the chat server: ")
 web_server_common_name = input("Enter common name for the web server: ")
 
-# Step 2: Generate private keys
+# Add the IP addresses and names of the CN in /etc/hosts.
+subprocess.call(["sudo", "sed", "-i", "5i 10.0.0.5        " + chat_server_common_name, "/etc/hosts"])
+subprocess.call(["sudo", "sed", "-i", "6i 10.0.1.5        " + web_server_common_name, "/etc/hosts"])
+
+# Step 2: Generate private keys for each certificate
+# Replaced the key_filename in the subprocess run statement with cakey.pem and matched it with Lab 6A Step 8.
 def generate_private_key(key_filename):
-    subprocess.run(["sudo", "openssl", "genrsa", "-out", key_filename, "2048"])
+    subprocess.run(["sudo", "openssl", "genrsa", "-aes256", "-out", "cakey.pem", "2048"])
 
 chat_server_key_filename = "chatserver-key.pem"
 web_server_key_filename = "webserver-key.pem"
@@ -22,13 +27,14 @@ web_server_key_filename = "webserver-key.pem"
 generate_private_key(chat_server_key_filename)
 generate_private_key(web_server_key_filename)
 
-# Step 3: Generate certificate signing requests
-def generate_certificate_signing_request(key_filename, csr_filename, common_name):
-    subprocess.run(["sudo", "openssl", "req", "-new", "-config", "/etc/ssl/openssl.cnf", "-key", key_filename, "-out", csr_filename, "-subj", f"/C=US/ST=CA/L=Seaside/O=CST311/OU=Networking/CN={common_name}"])
+# Step 3: Generate certificate signing requests using cakey.pem.
+# Removed the key_filename argument and instead used cakey.pem.
+def generate_certificate_signing_request(csr_filename, common_name):
+    subprocess.run(["sudo", "openssl", "req", "-x509", "-new", "-nodes", "-key", "cakey.pem", "-sha256","-days", "1825", "-out", csr_filename, "-subj", f"/C=US/ST=CA/L=Seaside/O=CST311/OU=Networking/CN={common_name}"])
 
 chat_server_csr_filename = "chatserver-cert.csr"
 web_server_csr_filename = "webserver-cert.csr"
 
-generate_certificate_signing_request(chat_server_key_filename, chat_server_csr_filename, chat_server_common_name)
-generate_certificate_signing_request(web_server_key_filename, web_server_csr_filename, web_server_common_name)
-
+# Removed the key_filename argument.
+generate_certificate_signing_request(chat_server_csr_filename, chat_server_common_name)
+generate_certificate_signing_request(web_server_csr_filename, web_server_common_name)
