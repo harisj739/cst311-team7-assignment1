49bbc5d8bb9f9d50099ad72f8b551c405ddf3b8d
Haris <126624444+harisj739@users.noreply.github.com>
2023-10-20 16:11:22 -0700

    Finished legacy_network.py
    I believe this should be the final version, everything works as of right now.

--- TPA4/legacy_network.py
+++ TPA4/legacy_network.py
@@ -85,11 +85,11 @@ def myNetwork():
     # these windows need to be created after the entire network is created -- keldin
     #using makeTerm to open individual terminal windows
     # Update: Removed the cmd parameter for h1, h3, & h4 to enable them to display along with h2.
-    makeTerm(h1, title='Node', term='xterm', display=None)
-    makeTerm(h2, title='Node', term='xterm', display=None, cmd='python3 CST311/tlswebserver.py')
-    makeTerm(h3, title='Node', term='xterm', display=None)
-    makeTerm(h4, title='Node', term='xterm', display=None)
-
+    makeTerm(h1, title='Node', term='xterm', display=None, cmd='python3 PA4_Chat_Client_Team7.py; bash')
+    makeTerm(h2, title='Node', term='xterm', display=None, cmd='python3 CST311/tlswebserver.py; bash')
+    makeTerm(h3, title='Node', term='xterm', display=None, cmd='python3 PA4_Chat_Client_Team7.py; bash')
+    makeTerm(h4, title='Node', term='xterm', display=None, cmd='python3 PA4_Chat_Server_Team7.py; bash') 
+    
     CLI(net)
     net.stop()
     # after you exit mininet, also close all the active terminal windows -- keldin
