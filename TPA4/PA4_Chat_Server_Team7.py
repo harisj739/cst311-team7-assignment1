3be4216781b53edd4c7843acc7dccaa306228cd9
Haris <126624444+harisj739@users.noreply.github.com>
2023-10-20 19:56:50 -0700

    Updated PA4_Chat_Server_Team7.py

--- TPA4/PA4_Chat_Server_Team7.py
+++ TPA4/PA4_Chat_Server_Team7.py
@@ -11,7 +11,7 @@ import threading
 import ssl  # Import the SSL module
 
 # Server configuration
-HOST = '0.0.0.0'  # Listen on all available network interfaces
+HOST = '10.0.1.2'  # Listen on all available network interfaces
 PORT = 2223  # Port for the chat server
 
 # List to store connected clients
@@ -34,6 +34,7 @@ def handle_client(client_socket):
     while True:
         try:
             message = client_socket.recv(1024)
+            print(f"{message}")
             if not message:
                 # Remove disconnected clients
                 remove(client_socket)
@@ -49,11 +50,11 @@ def remove(client_socket):
 
 def main():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((HOST, PORT)
+    server.bind(('', PORT))
     
     # Load the SSL certificate and key
     context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-    context.load_cert_chain(certfile="server_cert.pem", keyfile="server_key.pem")
+    context.load_cert_chain(certfile="chatserver-cert.pem", keyfile="chatserver-key.pem")
     
     # Wrap the server socket in the SSL context
     server = context.wrap_socket(server, server_side=True)
@@ -64,9 +65,11 @@ def main():
 
     while True:
         client_socket, client_address = server.accept()
+        secureConnSocket = context.wrap_socket(client_socket, server_side=True)
         clients.append(client_socket)
+
         print(f"Client {client_address[0]}:{client_address[1]} connected")
-        client_thread = threading.Thread(target=handle_client, args=(client_socket,))
+        client_thread = threading.Thread(target=handle_client, args=(secureConnsocket,))
         client_thread.start()
 
 if __name__ == "__main__":
