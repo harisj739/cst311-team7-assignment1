5cf67e5e5f689b4a99fcd61d52d90a330509e407
Haris <126624444+harisj739@users.noreply.github.com>
2023-10-20 21:16:14 -0700

    Updated PA4_Chat_Server_Team7.py
    This is the updated server.

--- TPA4/PA4_Chat_Server_Team7.py
+++ TPA4/PA4_Chat_Server_Team7.py
@@ -14,38 +14,27 @@ import ssl  # Import the SSL module
 HOST = '10.0.1.2'  # Listen on all available network interfaces
 PORT = 2223  # Port for the chat server
 
-# List to store connected clients
-clients = []
-
 # Load the SSL certificate and key
-context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-context.load_cert_chain("chatserver-cert.pem", "chatserver-key.pem")  # Certificate and file names
+# context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+# context.load_cert_chain("chatserver-cert.pem", "chatserver-key.pem")  # Certificate and file names
 
 def broadcast(message, client_socket):
-    for client in clients:
-        if client != client_socket:
-            try:
-                client.send(message)
-            except:
-                # Remove the client if unable to send a message
-                remove(client)
+       client_socket.send(message.encode())
+       # Close the client if unable to send a message
+       remove(client_socket)
 
 def handle_client(client_socket):
-    while True:
         try:
-            message = client_socket.recv(1024)
-            print(f"{message}")
+            message = client_socket.recv(1024).decode()
             if not message:
                 # Remove disconnected clients
                 remove(client_socket)
             else:
                 broadcast(message, client_socket)
         except:
-            continue
+            close(client_socket)
 
-def remove(client_socket):
-    if client_socket in clients:
-        clients.remove(client_socket)
+def close(client_socket):
         client_socket.close()
 
 def main():
@@ -55,9 +44,7 @@ def main():
     # Load the SSL certificate and key
     context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
     context.load_cert_chain(certfile="chatserver-cert.pem", keyfile="chatserver-key.pem")
-    
-    # Wrap the server socket in the SSL context
-    server = context.wrap_socket(server, server_side=True)
+   
 
     server.listen()
 
@@ -66,10 +53,9 @@ def main():
     while True:
         client_socket, client_address = server.accept()
         secureConnSocket = context.wrap_socket(client_socket, server_side=True)
-        clients.append(client_socket)
 
         print(f"Client {client_address[0]}:{client_address[1]} connected")
-        client_thread = threading.Thread(target=handle_client, args=(secureConnsocket,))
+        client_thread = threading.Thread(target=handle_client, args=(secureConnSocket,))
         client_thread.start()
 
 if __name__ == "__main__":
