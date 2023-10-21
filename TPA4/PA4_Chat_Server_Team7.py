b436e1d8c661634fd2d9c93e15a9f21a85d3f960
Haris <126624444+harisj739@users.noreply.github.com>
2023-10-21 15:49:38 -0700

    Updated PA4_Chat_Server_Team7.py
    Sends the response to one host but the other receives an empty response.

--- TPA4/PA4_Chat_Server_Team7.py
+++ TPA4/PA4_Chat_Server_Team7.py
@@ -10,53 +10,72 @@ import socket
 import threading
 import ssl  # Import the SSL module
 
+# Configure logging
+import logging
+logging.basicConfig()
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+
 # Server configuration
 HOST = '10.0.1.2'  # Listen on all available network interfaces
 PORT = 2223  # Port for the chat server
 
-# Load the SSL certificate and key
-# context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-# context.load_cert_chain("chatserver-cert.pem", "chatserver-key.pem")  # Certificate and file names
+# The context is the TLS Protocol.
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+context.load_cert_chain(certfile="chatserver-cert.pem", keyfile="chatserver-key.pem")
 
-def broadcast(message, client_socket):
-       client_socket.send(message.encode())
-       # Close the client if unable to send a message
-       remove(client_socket)
-
-def handle_client(client_socket):
+def handle_client(thread, client_socket, secure_socket, addr, clients):
         try:
-            message = client_socket.recv(1024).decode()
-            if not message:
-                # Remove disconnected clients
-                remove(client_socket)
+            message = secure_socket.recv(1024).decode()
+            
+            # Log query information
+            log.info("Connected to client at " + str(addr) )
+  	    # makes first client to respond first in host_list and sets to X
+            if thread == 0:
+                clients.append(f"X : \"{message}\" ")  
             else:
-                broadcast(message, client_socket)
-        except:
-            close(client_socket)
+                clients.append(f"Y : \"{message}\" ")
 
-def close(client_socket):
-        client_socket.close()
+            log.info(f"Received query test \" {message} \" ")
+
+  	    #make sure list has two elements, if not insert short delay
+            while len(clients) < 2:
+                time.sleep(5)
+
+	    #combine client messages into single response
+            response = f"{clients[0]}, {clients[1]}"
+            secure_socket.send(response.encode())
+            secure_socket.close()
+            # client_socket.close()
+        except:
+            secure_socket.close()
+            client_socket.close()
 
 def main():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind(('', PORT))
     
-    # Load the SSL certificate and key
-    context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-    context.load_cert_chain(certfile="chatserver-cert.pem", keyfile="chatserver-key.pem")
+    clients = []
    
-
-    server.listen()
+    server.listen(5)
 
     print(f"Server is listening on port {PORT}")
-
-    while True:
-        client_socket, client_address = server.accept()
-        secureConnSocket = context.wrap_socket(client_socket, server_side=True)
-
-        print(f"Client {client_address[0]}:{client_address[1]} connected")
-        client_thread = threading.Thread(target=handle_client, args=(secureConnSocket,))
-        client_thread.start()
-
+    
+    try:
+         for i in range(2):
+    	        # When a client connects, create a new socket and record their address
+                client_socket, client_address = server.accept()
+                print("The i value is: ", i)
+                log.info("Connected to at " + str(client_address))
+        	# Uses the context to establish a secure TCP socket laced with TLS.
+                secureConnSocket = context.wrap_socket(client_socket, server_side=True)
+        	
+        	# Pass the new socket and address off to a connection handler function in a thread.
+      		# Here we create a thread for each secure TCP connection established:
+                threading.Thread(target=handle_client, args=(i, client_socket, secureConnSocket, client_address, clients)).start()
+                client_socket.close()
+    
+    finally: 
+    	 server.close()
 if __name__ == "__main__":
     main()
